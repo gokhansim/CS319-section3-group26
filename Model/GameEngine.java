19bd2166efb965dcfd46302e7810e1008fe412e2
furkankucukbay <kucukbays@gmail.com>
2016-12-16 11:17:13 +0300

    GameEngine class is added. Ready to be pulled

---
+++ Model/GameEngine.java
@@ -0,0 +1,36 @@
+package Model;
+
+public class GameEngine {
+
+	private GameBody[][] map;
+	private CollisionManager colMngr;
+
+
+	public void addScore( int scoreGiven){
+
+	}
+
+	public void createGameBody(GameBody body) {
+
+	}
+
+	/**
+	 * 
+	 * @param body
+	 */
+	public void destroyGameBody(GameBody body) {
+		// TODO - implement GameEngine.destroyGameBody
+		throw new UnsupportedOperationException();
+	}
+
+	public GameBody[][] getMap() {
+		// TODO - implement GameEngine.getMap
+		throw new UnsupportedOperationException();
+	}
+
+	public void updateMap() {
+		// TODO - implement GameEngine.updateMap
+		throw new UnsupportedOperationException();
+	}
+
+}
