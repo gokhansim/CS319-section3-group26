a8baf29041111f2f0ddd6f5e1a8ecd6a109a770f
Irem Yuksel <irm.yksl@gmail.com>
2016-12-19 22:56:26 +0200

    Done

--- Controller/HighScoreManager.java
+++ Controller/HighScoreManager.java
@@ -1,23 +1,76 @@
 package Controller;
 
-import java.util.ArrayList;
+import java.io.*;
+import java.util.*;
 
 public class HighScoreManager {
 
-	private ArrayList scoreList;
-
-	/**
-	 * 
-	 * @param score
-	 */
+	private ArrayList<Integer> scoreList;
+	private File file;
+	
+	public HighScoreManager(){
+		scoreList = new ArrayList<Integer>();
+		file = new File("C:/Users/HP/Downloads/test.txt"); //must be changed
+	}
+	
+	//this method takes the score of the player and adds it to the high scores if it is in the top 5
 	public void registerHighScore(int score) {
-		// TODO - implement HighScoreManager.registerHighScore
-		throw new UnsupportedOperationException();
+		
+		this.readHighScore();
+		boolean added = false;
+		if(scoreList.size() != 0)
+			for(int s : scoreList){
+				if(s == score)
+					break;
+				if(score > s){
+					scoreList.add(scoreList.indexOf(s), score);
+					if(scoreList.size() == 6)
+						scoreList.remove(5); //since only the 5 highest scores will be kept
+					added = true;
+					break;
+				}
+			}
+		else {
+			scoreList.add(score);
+			added = true;
+		}
+		if(added){
+	
+		try{
+			FileOutputStream fos = new FileOutputStream(file);
+			ObjectOutputStream oos = new ObjectOutputStream(fos);
+			oos.writeObject( scoreList );
+			oos.close();
+			fos.close();
+			
+		}
+		catch (IOException e){}
+		
+		}
 	}
 
-	public void showHighScore() {
-		// TODO - implement HighScoreManager.showHighScore
-		throw new UnsupportedOperationException();
+	//returns the top 5 high scores
+	public ArrayList<Integer> showHighScore() {
+		this.readHighScore();
+		return scoreList;
 	}
-
+	
+	//for updating scoreList each time
+	private void readHighScore(){
+		try{
+			FileInputStream fis = new FileInputStream(file);
+			ObjectInputStream ois = new ObjectInputStream(fis);
+		
+			try {
+				scoreList = (ArrayList<Integer>) ois.readObject();
+			}
+			catch (ClassNotFoundException a){}
+		
+			ois.close();
+		}
+		
+		catch (IOException e){}
+		
+	}
+	
 }
