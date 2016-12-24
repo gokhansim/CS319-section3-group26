eec2a90cbce1f86148ccbef63e20c8308f677f55
furkankucukbay <kucukbays@gmail.com>
2016-12-24 14:36:42 +0300

    now getting points from enemyTank. enemies can no longer shoot each other

--- Model/CollisionManager.java
+++ Model/CollisionManager.java
@@ -7,21 +7,33 @@ public class CollisionManager {
 		if( GameBody1 instanceof Bullet) {
 			if ( GameBody2 instanceof SteelWall) {
 				return false;
-			} else if ( GameBody2 instanceof BrickWall) {
+			}
+			else if ( GameBody2 instanceof BrickWall) {
 				if( ((Bullet)GameBody1).getShooter() == 1){
 					engine.addScore( ((BrickWall) GameBody2).getScore() );
 				}
 				return ((BrickWall)GameBody2).getDestroyed(engine);
-			} else if ( GameBody2 instanceof IronWall && ((IronWall) GameBody2).getHitsToDestroy() == 1){
+			}
+			else if ( GameBody2 instanceof IronWall && ((IronWall) GameBody2).getHitsToDestroy() == 1){
+				if( ((Bullet)GameBody1).getShooter() == 1){
+					engine.addScore( ((BrickWall) GameBody2).getScore() );
+				}
 				return ((IronWall)GameBody2).getDestroyed(engine);
-			} else if ( GameBody2 instanceof EnemyTank){
-				return ((EnemyTank)GameBody2).getDestroyed(engine);
-			} else if ( GameBody2 instanceof SacredObject){
+			}
+			else if ( GameBody2 instanceof EnemyTank){
+				if( ((Bullet)GameBody1).getShooter() == 1){
+					engine.addScore( ((EnemyTank) GameBody2).getScoreGiven() );
+					isDestroyed = ((EnemyTank)GameBody2).getDestroyed(engine);
+				}
+				return isDestroyed;
+			}
+			else if ( GameBody2 instanceof SacredObject){
 				System.out.println("The sacred object is hit! Game over.");
 				engine.isGameOver = true; //!(((SacredObject)GameBody2).getIsDestroyed());
 				engine.cleanMap();
 				return ((SacredObject)GameBody2).getDestroyed(engine);
-			} else if (GameBody2 instanceof PlayerTank) {
+			}
+			else if (GameBody2 instanceof PlayerTank) {
 				System.out.println("You are hit! Remaining lives: " + engine.getPlayerTank().getCurrentLives());
 				if (engine.getPlayerTank().getCurrentLives() == 0 && engine.getPlayerTank().getShield() == 0) {
 					engine.isGameOver = true;
